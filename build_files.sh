986a22846cb68d3e80d1aa3fb3d3a09f9217c261
ragupathy-v <ragupathyv02@gmail.com>
2025-01-08 11:42:28 +0530

    code is commited

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,6 @@
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
+
+
+which python3 || { echo "Python3 not found"; exit 1; }
+which pip3 || { echo "Pip3 not found"; exit 1; }
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput
